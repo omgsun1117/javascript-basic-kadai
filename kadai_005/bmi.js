19504a59c818f0c58129263db402b310c6b819c6
omgsun1117 <136788059+omgsun1117@users.noreply.github.com>
2023-11-25 11:58:08 +0900

    Add files via upload

---
+++ kadai_005/bmi.js
@@ -0,0 +1,14 @@
+// 変数の宣言
+let taiju = 68;
+let shintyo = 1.7;
+let result = taiju/(shintyo*shintyo);
+
+// 値の代入
+weight = '体重(kg)';
+
+// 変数の宣言・値の代入
+let userNumber = 68;
+
+ // コンソールへの出力
+ console.log(result);
+
